3549e2e5bdc0c0b38ceda7406a3f587d0c3bd554
abdullah4a <abdullahkhan4a@gmail.com>
2022-01-04 14:55:55 +0500

    Initial commit

---
+++ src/test/java/com/starter/startingspring/StartingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.startingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
