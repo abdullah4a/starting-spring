1a9457de071a17afd098aff6ccbcedadeefc2e79
abdullah4a <abdullahkhan4a@gmail.com>
2022-01-04 16:41:25 +0500

    Adding Entities

---
+++ src/main/java/com/starter/StartingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.starter.startingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingSpringApplication.class, args);
+	}
+
+}
